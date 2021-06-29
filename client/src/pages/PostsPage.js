bb7f3426b7fa17d7e145ad04b5de6e2fc66719e7
Raiyan Shamim <razash139@gmail.com>
2021-06-29 17:09:50 +0300

    delete post

--- client/src/pages/PostsPage.js
+++ client/src/pages/PostsPage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { gql, useQuery } from '@apollo/client';
+import React, { useState, useEffect } from 'react';
+import { gql, useQuery, useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 
 import Loading from '../components/Loading';
@@ -15,13 +15,36 @@ const GET_POSTS = gql`
 	}
 `;
 
+const DELETE_POST = gql`
+	mutation DeletePost($id: Int!) {
+		deletePost(id: $id) {
+			id
+			title
+			body
+		}
+	}
+`;
+
 const PostsPage = () => {
 	const { loading, error, data } = useQuery(GET_POSTS);
+	const [deletePost] = useMutation(DELETE_POST, {
+		onCompleted: (data) => setPosts(data.deletePost),
+	});
+
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		if (!loading && !error && data) {
+			setPosts(data.getPosts);
+		}
+	}, [loading, error, data]);
+
+	const onDeletePost = (id) => {
+		deletePost({ variables: { id } });
+	};
 
 	if (loading) return <Loading />;
 	if (error) return <Error />;
-
-	const { getPosts: posts } = data;
 
 	return (
 		<div>
@@ -29,9 +52,14 @@ const PostsPage = () => {
 				<h1>Posts</h1>
 				{posts.map((post) => (
 					<div key={post.id}>
-						<h2>{post.title}</h2>
+						<h2>
+							{post.id} - {post.title}
+						</h2>
 						<p>{post.body}</p>
 						<Link to={`/posts/${post.id}`}>View Full</Link>
+						<button onClick={() => onDeletePost(post.id)}>
+							Delete
+						</button>
 					</div>
 				))}
 			</div>
